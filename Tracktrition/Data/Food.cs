31b42b726c881aec67f422559ab71f47a9d7a48a
@woehrjo <116088764+woehrjo@users.noreply.github.com>
2022-12-09 13:02:26 +0100

    Added Food Class

---
+++ Tracktrition/Data/Food.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracktrition.Data
+{
+    class Food
+    {
+        string name;
+        int calories;
+        double fat;
+        double protein;
+        double carbs;
+
+        Food(string name, int calories, double fat, double protein, double carbs)
+        {
+            this.name = name;
+            this.calories = calories;
+            this.fat = fat;
+            this.protein = protein; 
+            this.carbs = carbs;
+        }
+
+    }
+}
