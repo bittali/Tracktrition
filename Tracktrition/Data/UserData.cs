dcefb906ae1afa944a71b38a51fa8f5c65c31bbf
@woehrjo <116088764+woehrjo@users.noreply.github.com>
2022-12-09 12:57:21 +0100

    Added UserData Class

---
+++ Tracktrition/Data/UserData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracktrition.User
+{
+    internal class UserData
+    {
+
+        string name;
+        char sex;
+        int age;
+        double weight;
+        double height;
+        double bmi;
+
+        UserData(string name, double weight, double height)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.height = height;
+            this.bmi = weight / (height*height);    
+        }
+    }
+}
