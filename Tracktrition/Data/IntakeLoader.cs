4ef8c205056f3394089233b689273cb7e996f3c9
@woehrjo <116088764+woehrjo@users.noreply.github.com>
2023-05-28 23:14:37 +0200

    Added DailyIntake Loader

---
+++ Tracktrition/Data/IntakeLoader.cs
@@ -0,0 +1,71 @@
+﻿namespace Tracktrition.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class DailyIntakeLoader
+{
+    private const string DailyIntakeFileName = "daily_intake.csv";
+
+    public static List<DailyIntake> ReadDailyIntakeFromFile(string username)
+    {
+        string UserDailyIntakeFileName = username + "_" + DailyIntakeFileName;
+
+        List<DailyIntake> dailyIntakes = new List<DailyIntake>();
+
+
+        if (!File.Exists(UserDailyIntakeFileName))
+        {
+            File.Create(UserDailyIntakeFileName).Close(); // Create the file if it doesn't exist
+        }
+
+        using (StreamReader reader = new StreamReader(UserDailyIntakeFileName))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line); // DEBUGGING
+                string[] intakeData = line.Split(',');
+
+                if (intakeData.Length == 6)
+                {
+                    DateTime date = DateTime.Parse(intakeData[0]);
+                    int calories = int.Parse(intakeData[1]);
+                    double carbs = double.Parse(intakeData[2]);
+                    double protein = double.Parse(intakeData[3]);
+                    double fat = double.Parse(intakeData[4]);
+
+                    DailyIntake dailyIntake = new DailyIntake(date)
+                    {
+                        calories = calories,
+                        carbs = carbs,
+                        protein = protein,
+                        fat = fat
+                    };
+
+                    dailyIntakes.Add(dailyIntake);
+                }
+            }
+        }
+
+        return dailyIntakes;
+    }
+
+    public static bool SaveDailyIntakeToFile(List<DailyIntake> dailyIntakes, string username)
+    {
+        string UserDailyIntakeFileName = username + "_" + DailyIntakeFileName;
+
+        using (StreamWriter writer = new StreamWriter(UserDailyIntakeFileName))
+        {
+            foreach (DailyIntake dailyIntake in dailyIntakes)
+            {
+                string line = $"{dailyIntake.date},{dailyIntake.calories},{dailyIntake.carbs},{dailyIntake.protein},{dailyIntake.fat}";
+                writer.WriteLine(line);
+            }
+        }
+
+        return true;
+    }
+}
+
